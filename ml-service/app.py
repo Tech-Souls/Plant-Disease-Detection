c80a7d7314afff14f33d6bfff483d4f6f256bdf1
Aown-Iqbal <aowniqbal708@gmail.com>
2026-02-09 16:18:28 +0500

    Update app.py

--- ml-service/app.py
+++ ml-service/app.py
@@ -24,7 +24,7 @@ app = FastAPI(title="Plant Disease Detection API")
 
 ALLOWED_ORIGINS = [
     "https://plant-dd.vercel.app",
-    "https://plants-frontend-c2fq.vercel.app,
+    "https://plants-frontend-c2fq.vercel.app",
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
